2305ea903ba737848e438df7e28419238576c18f
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2020-08-04 13:18:32 -0700

    first upload, her name is shelia
    be sweet to sheila, she is my first python exercise.  she wants to know how years you have been on this planet.

---
+++ hello.py
@@ -0,0 +1,14 @@
+#this program greats u & asks for ur name
+
+print('Hello Beautiful!')
+print('what is ur name?') 
+name = input()
+print('Enchante, ' + name)
+print('did you know the number of letters in ur name is:')
+print(len(name))
+print('how young are you?')
+age=input()
+print('in 1 year, u will have ' + str(int(age) + 1) + ' years on this planet')
+print('any last words?')
+words = input()
+print('goodbye ' + name)
