58580853b1dae8f23f4520ab1e87cf60b9b975ee
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2020-08-04 16:34:58 -0700

    the final project
    so what do you think? isnt she lovely? just a little program who wants to talk about breakfast

--- elif.py
+++ elif.py
@@ -4,11 +4,17 @@ bfast = input("So tell me, did you eat breakfast this morning? ").lower()
 
 if bfast == 'y':
     food = input('Prada ya! what did you eat?').lower()
-    if food == ['a','s','d','f','g','h','j','k','l']:
+    if food[0] in ['a','s','d','f','g','h','j','k','l']:
             print('Yum, jelious. goodbye')
+    elif food[0] in ['q','w','e','r','t','y','u']:
+        print('there alot of calories in that')
+    elif food[0] in ['z']:
+        print('u ate zucchini??')
+    elif food[0] in ['x','c','v']:
+        print('zoomin in the foreign, got the engine roaring')
     else:
-        print('fsfaef')
-elif bfast == 'n'or bfast == 'N': 
+        print('Wow you ate a lot! Munchies???').lower()
+elif bfast == 'n': 
     print('Plz dont talk to me unless u have eaten breakfast. goodbye')
 else:
     print('i dont understand ur answer..')
